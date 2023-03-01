260d09cb9128f862dd90b9a760f1764670360bb9
prashant_handel <prashanthandel2501@gmail.com>
2023-03-01 12:23:14 +0530

    Final Responsive Site

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Services from './components/Services';
+
+
 import './styles/App.scss';
 import './styles/header.scss';
 import './styles/home.scss';
+import './styles/contact.scss';
+import './styles/mediaquery.scss';
 
 function App() {
   return (
@@ -12,6 +18,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
       </Routes>
       <Footer/>
     </Router>
